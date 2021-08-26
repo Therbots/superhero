16aa376e275585207d63e351f659bffa56067739
taylor herbots <therbots@live.com>
2021-08-26 18:55:53 -0500

    updated edit.html and edit path in urls.py

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -36,13 +36,13 @@ def edit(request, hero_id):
     if request.method == "POST":
         single_hero.name = request.POST.get('name')
         single_hero.alter_ego = request.POST.get('alter_ego')
-        single_hero.primary = request.POST.get('primary')
-        single_hero.secondary = request.POST.get('secondary')
-        single_hero.catchphrase = request.POST.get('catchphrase')
+        single_hero.primary_ability = request.POST.get('primary')
+        single_hero.secondary_ability = request.POST.get('secondary')
+        single_hero.catch_phrase = request.POST.get('catchphrase')
         single_hero.save()
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         context = {
-        'single_hero': single_hero
+            'single_hero': single_hero
         }
-        return render(request, 'superheroes/detail.html', context)
+        return render(request, 'superheroes/edit.html', context)
