f5b1d84e761f7657d31cef71da58fcbb91724a0e
taylor herbots <therbots@live.com>
2021-08-26 18:19:53 -0500

    updated edit function in views.py

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -31,15 +31,18 @@ def create(request):
     else:
         return render(request, 'superheroes/create.html')
 
-def edit(request):
+def edit(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
     if request.method == "POST":
-        name = request.POST.get('name')
-        alter_ego = request.POST.get('alter_ego')
-        primary = request.POST.get('primary')
-        secondary = request.POST.get('secondary')
-        catchphrase = request.POST.get('catchphrase')
-        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
-        new_hero.save()
+        single_hero.name = request.POST.get('name')
+        single_hero.alter_ego = request.POST.get('alter_ego')
+        single_hero.primary = request.POST.get('primary')
+        single_hero.secondary = request.POST.get('secondary')
+        single_hero.catchphrase = request.POST.get('catchphrase')
+        single_hero.save()
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
-        return render(request, 'superheroes/create.html')
+        context = {
+        'single_hero': single_hero
+        }
+        return render(request, 'superheroes/detail.html', context)
